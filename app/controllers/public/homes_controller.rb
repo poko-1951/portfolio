4e900d14437304f7ca330e9fb7c78f9709893c0b
poko-1951 <go.k0624@gmail.com>
2022-07-19 08:57:50 +0000

    [Add]ログイン中はトップページに遷移できないようにした

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,4 +1,18 @@
 class Public::HomesController < ApplicationController
+  before_action :user_signed_in
+  before_action :admin_signed_in
+
   def top
   end
+
+  private
+
+  def user_signed_in
+    redirect_to topics_path if user_signed_in?
+  end
+
+  def admin_signed_in
+    redirect_to admin_path if admin_signed_in?
+  end
+
 end
