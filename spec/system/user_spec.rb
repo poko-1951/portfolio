2fc32ca2d463389632f7ee2180665d8b3df9f6e2
poko-1951 <go.k0624@gmail.com>
2022-08-01 05:18:04 +0000

    [Add]rspec_トップページ

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :system do
     let!(:user) { create(:user) }
     describe "ログイン前" do
       describe "ユーザー新規登録" do
-        context "正常" do
+        context "成功" do
           it "作成完了" do
             visit new_user_registration_path
             fill_in "アカウント名", with: "test_sign_upuser"
@@ -18,10 +18,24 @@ RSpec.describe User, type: :system do
             expect(page).to have_content 'ログアウト'
           end
         end
+
+        context "失敗" do
+          it "作成失敗" do
+            visit new_user_registration_path
+            fill_in "アカウント名", with: " "
+            fill_in "メールアドレス", with: " "
+            fill_in "パスワード（6桁以上）", with: " "
+            fill_in "確認用パスワード", with: " "
+            click_button "登録する"
+            expect(current_path).to eq users_path
+            visit current_path
+            expect(current_path).to eq new_user_registration_path
+          end
+        end
       end
 
       describe "ユーザーログイン" do
-        context "正常" do
+        context "成功" do
          it "ログイン完了" do
           visit new_user_session_path
           fill_in "メールアドレス", with: user.email
@@ -31,10 +45,20 @@ RSpec.describe User, type: :system do
           expect(page).to have_content 'ログアウト'
          end
         end
+        context "失敗" do
+         it "ログイン失敗" do
+          visit new_user_session_path
+          fill_in "メールアドレス", with: " "
+          fill_in "パスワード", with: " "
+          click_button "ログイン"
+          expect(current_path).to eq new_user_session_path
+         end
+        end
+
       end
 
       describe "ゲストログイン" do
-        context "正常" do
+        context "成功" do
          it "ログイン完了" do
           visit root_path
           find('#guest_login').click
@@ -61,6 +85,22 @@ RSpec.describe User, type: :system do
             expect(page).to have_content "user1"
           end
         end
+      end
+    end
+  end
+  
+  describe "フッターチェック" do
+    context "正常に表示できるか" do
+      before do
+        visit root_path
+      end
+      it "利用規約" do
+        find('.terms_of_use').click
+        expect(page).to have_content "第1条（適用）"
+      end
+      it "プライバシーポリシー" do
+        find('.user_policy').click
+        expect(page).to have_content "第1条（個人情報）"
       end
     end
   end
