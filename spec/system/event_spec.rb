7faa69403695e8b005ed3253e78c213a3cd43bf2
poko-1951 <go.k0624@gmail.com>
2022-08-03 05:35:59 +0000

    [Add]RSpec_イベント登録

--- spec/system/event_spec.rb
+++ spec/system/event_spec.rb
@@ -6,13 +6,27 @@ RSpec.describe Event, type: :system do
     let!(:topic) { create(:topic, user: user) }
     let!(:acquaintance) { create(:acquaintance, user: user) }
     let!(:stock) { create(:stock, user: user, topic: topic, acquaintance: acquaintance) }
-    let!(:event) { create(:event, user: user, end: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)) }
+    # let!(:event) { create(:event, user: user, end: Faker::Time.between(from: DateTime.now - 1, to: DateTime.now)) }
 
     before do
       visit new_user_session_path
       fill_in "メールアドレス", with: user.email
       fill_in "パスワード", with: user.password
       click_button "ログイン"
+    end
+
+    describe "イベント登録のテスト", js: true do
+      let!(:time_count) { Time.now }
+      before do
+        visit events_path
+      end
+      it "イベント登録" do
+        first(".fc-daygrid-day-frame").click
+        fill_in "予定", with: "event"
+        expect {
+          find(".event_post_button").click
+        }.to change(Event.all, :count).by(1)
+      end
     end
 
     describe "カレンダー一覧のテスト" do
